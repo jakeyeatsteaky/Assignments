d03d52514477020f107d4a87ef9e1dea2e1f1f4e
Jake Baker <jakebaker@pop-os.localdomain>
2023-11-05 11:23:14 -0800

    update

--- src/main.c
+++ src/main.c
@@ -5,7 +5,19 @@
 
 int main (int argc, char** argv)
 {
-    run_lab0();
+    const uint32_t addThis = 42;
+    runStaticFunction(addThis);
+    runStaticFunction(addThis);
+    runStaticFunction(addThis);
+
+    // Since the function is static, its scope is local only to 
+    // the Lab_0.c translation unit.  
+    //
+    // Since the variable inside the function is static (baseNum) 
+    // it will maintain its value across successive calls to the function
+    // regardless of when that function goes out of scope
+
+
     return 0;
 }
 
